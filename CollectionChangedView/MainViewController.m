81f5a79be338819a4e108cc19728d52b62b0e1e5
StephenZhuang <379128008@qq.com>
2014-05-15 14:44:18 +0800

    add collection
    add collection

--- CollectionChangedView/MainViewController.m
+++ CollectionChangedView/MainViewController.m
@@ -29,6 +29,17 @@
     // Do any additional setup after loading the view.
 }
 
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    return cell;
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
