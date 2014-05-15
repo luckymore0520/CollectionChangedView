cfcbcfa61cc35d0559f33d61a93f3777f831a32c
StephenZhuang <379128008@qq.com>
2014-05-15 14:26:50 +0800

    import
    import project

---
+++ CollectionChangedView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionChangedView
+//
+//  Created by Stephen Zhuang on 14-5-14.
+//  Copyright (c) 2014年 udows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionChangedViewTests/CollectionChangedViewTests.m
@@ -0,0 +1,34 @@
+//
+//  CollectionChangedViewTests.m
+//  CollectionChangedViewTests
+//
+//  Created by Stephen Zhuang on 14-5-14.
+//  Copyright (c) 2014年 udows. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CollectionChangedViewTests : XCTestCase
+
+@end
+
+@implementation CollectionChangedViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
